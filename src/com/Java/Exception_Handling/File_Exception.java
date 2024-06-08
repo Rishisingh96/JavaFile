c7781801eac11c50dc2bf3598bf560708d5b2ae4
Rishi singh <95511249+Rishisingh96@users.noreply.github.com>
2024-06-08 12:08:20 +0530

    Add files via upload

---
+++ src/com/Java/Exception_Handling/File_Exception.java
@@ -0,0 +1,29 @@
+package file_exception;
+import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class File_Exception {
+    public static void main(String[] args) throws IOException {
+          String name;
+          // Write String
+         /* Scanner sc  = new Scanner(System.in);
+          System.out.println("Enter your name: ");
+          name = sc.nextLine();
+        
+          //FileWriter use for Writing String name
+          
+          FileWriter file = new FileWriter("Rishi/FileWriter.txt");
+          file.write(name);
+          file.close();
+          System.out.println("Your File is Created.........and Save data..........");
+    */
+          //FileReader use for Reading files
+          FileReader fr = new FileReader("Rishi/FileWriter.txt");
+          int st;
+          while((st=fr.read())!=-1)
+          {
+              System.out.println("Your ASCII value is "+ st +" Your charactor = "+ (char)st);
+          }  
+    }
+}
