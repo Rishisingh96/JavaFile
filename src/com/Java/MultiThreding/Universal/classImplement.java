cce929632130158cfdd8c6725680956afac0cf38
Rishi singh <95511249+Rishisingh96@users.noreply.github.com>
2024-06-13 13:40:13 +0530

    Add files via upload

---
+++ src/com/Java/MultiThreding/Universal/classImplement.java
@@ -0,0 +1,53 @@
+package Multithreding3;
+
+class Student {
+    String name;
+    int rollno;
+    int marks;
+
+    public Student(String name, int rollno, int marks) {
+        this.name = name;
+        this.rollno = rollno;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", rollno=" + rollno + ", marks=" + marks + "]";
+    }
+}
+
+class Facalty {
+    String name; 
+    String NameofFild;
+    String Address;
+
+    public Facalty(String name, String nameofFild, String address) {
+        this.name = name; 
+        NameofFild = nameofFild;
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Facalty [NameofFild=" + NameofFild + ", Address=" + Address + "]";
+    }
+}
+
+public class classImplement {
+    public static void main(String[] args) {
+        Student s = new Student("Rishi", 32, 72);
+        Facalty f = new Facalty("Raj", "C", "Indore, bholaram");
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(s.toString());
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println(f.toString());
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
