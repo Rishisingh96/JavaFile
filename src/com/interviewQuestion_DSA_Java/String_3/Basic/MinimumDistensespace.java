0ac28272d42486362a6249d0b5a0c4617bc49a2e
Rishi singh <95511249+Rishisingh96@users.noreply.github.com>
2024-06-27 13:13:45 +0530

    Add files via upload

---
+++ src/com/interviewQuestion_DSA_Java/String_3/Basic/MinimumDistensespace.java
@@ -0,0 +1,31 @@
+package rishi.Java.StringQuestion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumDistensespace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int size = sc.nextInt();
+//        String st[] = new String[5];
+//        for(int i=0; i<5; i++){
+//            st[i] = sc.nextLine();
+//        }
+        String k[] = {"Rahul","Mohit","Rohit"};
+        String s = "Mohitr";
+       // System.out.println(Arrays.binarySearch(k,s));
+        if(Arrays.binarySearch(k,s) ==1){
+            System.out.println("Element found");
+        }else {
+            System.out.println("Element is wrong");
+        }
+
+//        for(String eme : k){
+//            if(eme == s){
+//                System.out.println("Element found");
+//            }
+//          //  System.out.println(eme);
+//        }
+
+    }
+}
